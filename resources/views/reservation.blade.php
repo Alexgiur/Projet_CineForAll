c147ce4d3a112877a097dc5206407c5851d92355
NessimAboodi <nessim.aboodi@lamache.org>
2025-12-10 10:50:48 +0100

    Update reservation.blade.php

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Réservation - CineForAll</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="{{ asset('Css/style.css') }}">
 </head>
 <body>
 
