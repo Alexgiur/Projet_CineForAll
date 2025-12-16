4aaf64c992771097776606352767b47eb03cba9f
Lucas020869 <uriot.lucas@gmail.com>
2025-12-16 10:45:25 +0100

    modife films

--- database/migrations/2025_12_09_150203_create_films_table.php
+++ database/migrations/2025_12_09_150203_create_films_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('film', function (Blueprint $table) {
+        Schema::create('films', function (Blueprint $table) {
             $table->id('IdFilm');
             $table->string('TitreFilm');
             $table->integer('LongueurFilm');
