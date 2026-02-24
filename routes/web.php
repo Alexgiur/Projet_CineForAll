036aa42c41c0e6ed59931994f18c5f530396b186
Alexgiur <alxntm@gmail.com>
2026-02-24 16:07:17 +0100

    Routes, tentative de correction mdrrr

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,11 @@ use App\Http\Middleware\IsAdmin;
 
 /* 1. Page d'accueil */
 
-
+Route::get('/', function () {return view('welcome');})->name('home');
 
 /* 2. Routes Administrateur (Uniquement rôle = 1)*/
 Route::middleware(['auth', IsAdmin::class])->group(function () {
     // Génère les routes create, store, edit, update, destroy
-    Route::get('/', function () {return view('welcome');})->name('home');
     Route::get('films', [FilmController::class, 'index'])->name('films.index');
     Route::get('films/{id}', [FilmController::class, 'show'])->name('films.show');
     Route::get('createfilm', [FilmController::class, 'create'])->name('films.create');
@@ -30,6 +29,15 @@ Route::middleware(['auth', IsAdmin::class])->group(function () {
 // Tout le monde peut voir la liste (index) et les détails (show)
 //Route::resource('films', [FilmController::class, 'show'])->only(['index', 'show']);
 //Route::resource('personnes', PersonneController::class)->only(['index', 'show']);
+Route::middleware(['auth', IsAdmin::class])->(function (){
+    Route::get('personnes', [PersonneController::class, 'index'])->name('personnes.index');
+    Route::get('personnes/{id}', [PersonneController::class, 'show'])->name('personnes.show');
+    Route::get('createpersonnes' [PersonneController::class, 'create'])->name('personnes.create');
+    Route::post('store/personnes' [PersonneController::class, 'store'])->name('personnes.store');
+    Route::get('/personnes/{id}/edit', [FilmController::class, 'edit'])->name('personnes.edit');
+    Route::patch('update/film', [PersonneController::class, 'update'])->name('personnes.update');
+    Route::delete('destroy/film/{id}', [PersonneController::class, ])->name('personnes.destroy');
+});
 
 
 /* 4. Authentification */
